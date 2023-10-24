a2fca11e5b119e8b262587012ddf98e624fe9179
KatjaSi <katja.sivertsen@gmail.com>
2023-10-24 22:50:34 +0200

    ssl

---
+++ downstream_models.py
@@ -0,0 +1,56 @@
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader
+
+from pointSSL import POINT_SSL
+from data import load_data
+
+from data_handling import  ModelNet
+
+class PointCloudClassifier(nn.Module):
+    def __init__(self, input_dim, num_classes):
+        super(PointCloudClassifier, self).__init__()
+        self.fc1 = nn.Linear(input_dim, 256)
+        self.fc2 = nn.Linear(256, 128)
+        self.fc3 = nn.Linear(128, num_classes)
+
+    def forward(self, x):
+        x = torch.relu(self.fc1(x))
+        x = torch.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+
+def main():
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    device = torch.device(device)
+    point_ssl = POINT_SSL()
+    point_ssl.load_state_dict(torch.load('checkpoints/models/point_ssl_1000.t7'))
+    point_ssl.eval()
+
+    train_points, train_labels = load_data("train")
+    #test_points, test_labels = load_data("test")
+
+    train_set = ModelNet(train_points, train_labels, set_type="test", num_points=2048)
+    train_loader = DataLoader(dataset=train_set, num_workers=4, batch_size=32, shuffle=True)
+    #test_set = ModelNet(test_points, test_labels, set_type="test", num_points=2048)
+
+    #first_batch = next(iter(train_loader))
+    #print(len(first_batch))
+    
+   # model = model.double()
+    point_ssl = point_ssl.half().to(device)
+
+    for data, labels in (train_loader):
+        data = data.half().to(device)
+        data = data.permute(0, 2, 1)
+        embeddings, _ = point_ssl.__forward__(data)
+        print(embeddings.shape)
+        print(embeddings[0])
+        break
+
+    #embeddings = point_ssl.__forward__(first_batch.to(device))
+    
+
+if __name__ == '__main__':
+    main()
--- train_SSL.py
+++ train_SSL.py
@@ -4,6 +4,7 @@ import numpy as np
 import copy
 import torch.optim as optim
 from torch.utils.data import DataLoader
+from torch.optim.lr_scheduler import CosineAnnealingLR
 from torch.utils.data import Dataset
 from pytorch_metric_learning.losses import NTXentLoss
 from pointSSL import POINT_SSL
@@ -21,23 +22,38 @@ def train(model:POINT_SSL, train_loader:DataLoader, criterion,  optimizer, num_e
     model = model.half().to(device)
     model = nn.DataParallel(model)
 
+    learning_rate = optimizer.param_groups[0]['lr']
+    scheduler = CosineAnnealingLR(optimizer, num_epochs, eta_min=learning_rate/100)
 
     for epoch in range(num_epochs):
         running_loss = 0.0
         count = 0.0
         model.train()
-
         for x_prime, x in (train_loader):
+            batch_size = len(x_prime)
             x_prime = x_prime.half().to(device)
             x = x.half().to(device)
             x = x.permute(0, 2, 1)
             x_prime = x_prime.permute(0,2,1)
+            optimizer.zero_grad()
             x_prime_rep, x_rep, x_prime_projection, x_projection = model(x_prime, x)
-            embeddings = torch.cat((x_prime_projection, x_projection))
-            print(embeddings.shape)
+            projections = torch.cat((x_prime_projection, x_projection))
+            indices = torch.arange(0, x_prime_projection.shape[0])
+            labels = torch.cat((indices, indices))
+            loss = criterion(projections, labels)
+            loss.backward()
+            optimizer.step()  
+            scheduler.step()
+
+            count += batch_size
+            running_loss += loss.item() * batch_size
+
+        outstr = 'Epoch: %d, loss: %.6f' % (epoch, running_loss*1.0/count)
+        print(outstr)
 
 
-
+            
+            
 
 class ModelNetForSSL(Dataset):
     def __init__(self, data, num_points=1024, crop_percentage=0.4):
@@ -67,14 +83,16 @@ class ModelNetForSSL(Dataset):
 def main():
     train_points, _ = load_data("train")
     train_set = ModelNetForSSL(train_points, num_points=2048, crop_percentage=0.3)
-    train_loader = DataLoader(dataset=train_set, num_workers=4, batch_size=1024, shuffle=True)
+    train_loader = DataLoader(dataset=train_set, num_workers=4, batch_size=512, shuffle=True)
 
     model = POINT_SSL()
 
     loss = NTXentLoss(temperature = 0.1)
-    optimizer = optim.Adam(lr=0.001, params=model.parameters())
-    train(model, train_loader, loss, optimizer,1)
+    #optimizer = optim.Adam(lr=0.001, params=model.parameters())
+    optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9, weight_decay=5e-4)
+    train(model, train_loader, loss, optimizer,1000)
 
+    torch.save(model.state_dict(), 'checkpoints/models/point_ssl_1000.t7')
 
 
 if __name__ == '__main__':
